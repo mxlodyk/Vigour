b8358b8fe0ab4fc73902f6e3c4b2940858fd2274
mxlodyk <melodykimfl@gmail.com>
2024-07-04 15:30:39 +0800

    Initial Commit

---
+++ Vigour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vigour
+//
+//  Created by Melody Flavel on 4/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vigour/VigourApp.swift
@@ -0,0 +1,17 @@
+//
+//  VigourApp.swift
+//  Vigour
+//
+//  Created by Melody Flavel on 4/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct VigourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
